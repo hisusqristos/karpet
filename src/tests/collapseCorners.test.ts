23803ab54903d7abe4e26f2c6f561606cd36ca44
blasteroid <varyadaser@gmail.com>
2023-06-20 19:54:44 +0400

    :golf: collapseCorners

--- src/tests/collapseCorners.test.ts
+++ src/tests/collapseCorners.test.ts
@@ -24,12 +24,6 @@ const collapseCorners = (grid: string[][]) => {
     const gridWidth = grid.length
     const gridHeight = grid[0].length
 
-    const collapse = (value: string) => (i: number, j: number) => (grid: string[][]) => (grid: string[][]) => {
-        const collapsedRow = grid[i].splice(j, 1, value);
-        const collapsed: string[][] = grid.splice(i, 1, collapsedRow);
-        return collapsed
-    }
-
     const cornersCollapsed: string[][] = pipe(
         grid,
         collapse("top-left-corner")(0, 0),
