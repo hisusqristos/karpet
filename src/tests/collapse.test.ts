36ee091a865902196f18cc60ede793a61094d379
blasteroid <varyadaser@gmail.com>
2023-06-21 13:40:47 +0400

    :axe: :soap: no comments, no logs

--- src/tests/collapse.test.ts
+++ src/tests/collapse.test.ts
@@ -21,32 +21,7 @@ describe.only("collapse at given coordinates, with given value", () => {
         expect(result).toEqual(collapsedZeroZero)
     })
 
-    // test("collapse at 0", () => {
-    //     const grid = ["", "", ""]
-    //     const collapsedZeroZero = collapse("asd")(0)(grid)
-    //     const result = ["asd", "", ""]
-
-    //     expect(result).toEqual(collapsedZeroZero)
-    // });
-
-    // test("collapse at 2", () => {
-    //     const grid = ["", "", ""]
-    //     const collapsedZeroZero = collapse("asd")(2)(grid)
-    //     const result = ["", "", "asd"]
-
-    //     expect(result).toEqual(collapsedZeroZero)
-    // });
-
 })
-
-// const collapse = (value: string) => {
-//     return (i: number) => {
-//         return (grid: string[]): string[] => {
-//             const collapsed: string[] = grid.splice(i, 1, value);
-//             return collapsed;
-//         };
-//     };
-// };
 
 const collapse = (value: string) => {
     return (i: number, j: number) => {
@@ -55,8 +30,6 @@ const collapse = (value: string) => {
                 grid[i].splice(j, 1, value);
                 grid.splice(i, 1, grid[i]);
             }
-
-            console.log(grid);
             return grid;
         }
     }
