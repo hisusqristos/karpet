dd9ff0bb17bb076f652d64a8e7b1a32c69ef6d84
blasteroid <varyadaser@gmail.com>
2023-06-30 12:01:17 +0400

    :feet: template for rules

---
+++ src/RULES.ts
@@ -0,0 +1,13 @@
+const rules = {
+    topLeftCorner: { up: [""], right: [""], bottom: [""], left: [""] },
+    topRighttCorner: { up: [""], right: [""], bottom: [""], left: [""] },
+    bottomRightCorner: { up: [""], right: [""], bottom: [""], left: [""] },
+    bottomLeftCorner: { up: [""], right: [""], bottom: [""], left: [""] },
+    topEdge: { up: [""], right: [""], bottom: [""], left: [""] },
+    bottomEdge: { up: [""], right: [""], bottom: [""], left: [""] },
+    leftEdge: { up: [""], right: [""], bottom: [""], left: [""] },
+    rightEdge: { up: [""], right: [""], bottom: [""], left: [""] },
+    internal: { up: [""], right: [""], bottom: [""], left: [""] }
+}
+
+export { rules }
