515187cb4d99d16d3721af1d54f98b856e82b321
blasteroid <varyadaser@gmail.com>
2023-06-30 15:09:37 +0400

    :axe: rules for edges

--- src/RULES.ts
+++ src/RULES.ts
@@ -3,11 +3,11 @@ const rules = {
     topRighttCorner: { up: [""], right: [""], bottom: [""], left: [""] },
     bottomRightCorner: { up: [""], right: [""], bottom: [""], left: [""] },
     bottomLeftCorner: { up: [""], right: [""], bottom: [""], left: [""] },
-    topEdge: { up: [""], right: [""], bottom: [""], left: [""] },
-    bottomEdge: { up: [""], right: [""], bottom: [""], left: [""] },
-    leftEdge: { up: [""], right: [""], bottom: [""], left: [""] },
-    rightEdge: { up: [""], right: [""], bottom: [""], left: [""] },
-    internal: { up: [""], right: [""], bottom: [""], left: [""] }
+    topEdge: { up: [""], right: ["topEdge"], bottom: ["bottomEdge", "internal"], left: ["topEdge"] },
+    bottomEdge: { up: ["internal", "topEdge"], right: ["bottomEdge"], bottom: [""], left: ["bottomEdge"] },
+    leftEdge: { up: ["leftEdge"], right: ["leftEdge", "internal"], bottom: ["leftEdge"], left: [""] },
+    rightEdge: { up: ["rightEdge"], right: [""], bottom: ["rightEdge"], left: ["leftEdge", "internal"] },
+    internal: { up: ["topEdge", "internal"], right: ["rightEdge", "internal"], bottom: ["bottomEdge", "internal"], left: ["leftEdge", "internal"] }
 }
 
 export { rules }
