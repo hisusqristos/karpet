062dc4790a2d033fa1d7e4bbc8a8a6de66e56076
blasteroid <varyadaser@gmail.com>
2023-06-30 09:22:13 +0400

    :golf:

---
+++ src/__tests__/collapseCorners.test.ts
@@ -0,0 +1,28 @@
+import { collapseCorners } from "../hepers/collapse";
+
+describe.skip("place corner tiles in corners", () => {
+    test("2x2 grid", () => {
+        const grid: string[][] = [['', ''], ['', '']];
+        const result = [["top-left-corner", "top-right-corner"], ["bottom-left-corner", "bottom-right-corner"]]
+
+        const withCorners = collapseCorners(grid);
+        expect(withCorners).toEqual(result);
+    });
+
+    test("3x2 grid", () => {
+        const grid: string[][] = [['', '', ''], ['', '', '']];
+        const result = [["top-left-corner", "", "top-right-corner"], ["bottom-left-corner", "", "bottom-right-corner"]]
+
+        const withCorners = collapseCorners(grid);
+        expect(withCorners).toEqual(result);
+    })
+
+    test.skip("anything smaller than 2x2 is invalid", () => {
+        const grid: string[][] = [['', ''], ['']];
+        const result = [['', ''], ['']];
+
+        const withCorners = collapseCorners(grid);
+        expect(withCorners).toEqual(result);
+    });
+});
+
---
+++ src/__tests__/gridInit.test.ts
@@ -0,0 +1,12 @@
+import { gridInit } from "../hepers/gridInit"
+
+describe.only("initialize an empty grid", () => {
+    test("width 2 height 3", () => {
+        const grid = [["", ""], ["", ""], ["", ""]]
+        expect(grid).toEqual(gridInit(2, 3))
+    })
+    test("if width or height smaller than 2 return 2x2 grid", () => {
+        const grid = [["", ""], ["", ""]];
+        expect(grid).toEqual(gridInit(1, 0));
+    })
+})
