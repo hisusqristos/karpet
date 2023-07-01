a596221ed0db833257d7a82d8836cdef0d5c0a9b
blasteroid <varyadaser@gmail.com>
2023-07-01 09:30:39 +0400

    :feet: fix gridInit return type

--- src/helpers/gridInit.ts
+++ src/helpers/gridInit.ts
@@ -1,4 +1,4 @@
-const gridInit = (width: number, height: number): string[] => {
+const gridInit = (width: number, height: number): string[][] => {
     let row: string[] = new Array(Math.max(width, 2)).fill("")
     const grid = new Array(Math.max(height, 2)).fill(row);
     return grid;
