46313ba5a7964c0033fdf5a901383040090d8403
blasteroid <varyadaser@gmail.com>
2023-07-02 12:57:11 +0400

    complete rules

--- src/RULES.ts
+++ src/RULES.ts
@@ -1,12 +1,12 @@
 const rules = {
-    topLeftCorner: { up: [""], right: [""], bottom: [""], left: [""] },
-    topRighttCorner: { up: [""], right: [""], bottom: [""], left: [""] },
-    bottomRightCorner: { up: [""], right: [""], bottom: [""], left: [""] },
-    bottomLeftCorner: { up: [""], right: [""], bottom: [""], left: [""] },
-    topEdge: { up: [""], right: ["topEdge"], bottom: ["bottomEdge", "internal"], left: ["topEdge"] },
-    bottomEdge: { up: ["internal", "topEdge"], right: ["bottomEdge"], bottom: [""], left: ["bottomEdge"] },
-    leftEdge: { up: ["leftEdge"], right: ["leftEdge", "internal"], bottom: ["leftEdge"], left: [""] },
-    rightEdge: { up: ["rightEdge"], right: [""], bottom: ["rightEdge"], left: ["leftEdge", "internal"] },
+    topLeftCorner: { up: [""], right: ["topEdge", "topRightCorner"], bottom: ["leftEdge", "bottomLeftCorner"], left: [""] },
+    topRighttCorner: { up: [""], right: [""], bottom: ["rightEdge", "bottomRightCorner"], left: ["topEdge", "topLeftCorner"] },
+    bottomRightCorner: { up: ["rightEdge", "topRightCorner"], right: [""], bottom: [""], left: ["bottomEdge", "bottomLeftCorner"] },
+    bottomLeftCorner: { up: ["leftEdge", "topLeftCorner"], right: ["bottomEdge", "bottomRightEdge"], bottom: [""], left: [""] },
+    topEdge: { up: [""], right: ["topEdge", "topRighttCorner"], bottom: ["bottomEdge", "internal"], left: ["topLeftCorner", "topEdge"] },
+    bottomEdge: { up: ["internal", "topEdge"], right: ["bottomRightCorner", "bottomEdge"], bottom: [""], left: ["bottomEdge", "bottomLeftCorner"] },
+    leftEdge: { up: ["leftEdge", "topLeftEdge"], right: ["rightEdge", "internal"], bottom: ["leftEdge", "bottomLeftEdge"], left: [""] },
+    rightEdge: { up: ["rightEdge", "topRightCorner"], right: [""], bottom: ["rightEdge", "bottomRightCorner"], left: ["leftEdge", "internal"] },
     internal: { up: ["topEdge", "internal"], right: ["rightEdge", "internal"], bottom: ["bottomEdge", "internal"], left: ["leftEdge", "internal"] }
 }
 
