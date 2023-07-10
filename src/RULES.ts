b0b7fd2b4faa95bd1d5370e698722d5487cae484
hisusqristos <varyadaser@gmail.com>
2023-07-10 12:03:31 +0400

    :axe: fix mispelled tiles

--- src/RULES.ts
+++ src/RULES.ts
@@ -1,21 +1,20 @@
 const allTiles: string[] = ["topLeftCorner", "topRighttCorner", "bottomRightCorner", "bottomLeftCorner", "topEdge", "bottomEdge", "leftEdge", "rightEdge", "internal"]
 
-type Tile = "topLeftCorner" | "topRighttCorner" | "bottomRightCorner" | "bottomLeftCorner" | "topEdge" | "bottomEdge" | "leftEdge" | "rightEdge" | "internal";
-type Sides = "up" | "right" | "bottom" | "left"
-type Rules = Record<string, Record<Sides, string[]>>
+type Tile = "topLeftCorner" | "topRightCorner" | "bottomRightCorner" | "bottomLeftCorner" | "topEdge" | "bottomEdge" | "leftEdge" | "rightEdge" | "internal";
+type Side = "up" | "right" | "bottom" | "left"
+type Rules = Record<string, Record<Side, Tile[]>>
 
 const rules: Rules = {
-    topLeftCorner: { up: [""], right: ["topEdge", "topRightCorner"], bottom: ["leftEdge", "bottomLeftCorner"], left: [""] },
-    topRighttCorner: { up: [""], right: [""], bottom: ["rightEdge", "bottomRightCorner"], left: ["topEdge", "topLeftCorner"] },
-    bottomRightCorner: { up: ["rightEdge", "topRightCorner"], right: [""], bottom: [""], left: ["bottomEdge", "bottomLeftCorner"] },
-    bottomLeftCorner: { up: ["leftEdge", "topLeftCorner"], right: ["bottomEdge", "bottomRightEdge"], bottom: [""], left: [""] },
-    topEdge: { up: [""], right: ["topEdge", "topRighttCorner"], bottom: ["bottomEdge", "internal"], left: ["topLeftCorner", "topEdge"] },
-    bottomEdge: { up: ["internal", "topEdge"], right: ["bottomRightCorner", "bottomEdge"], bottom: [""], left: ["bottomEdge", "bottomLeftCorner"] },
-    leftEdge: { up: ["leftEdge", "topLeftEdge"], right: ["rightEdge", "internal"], bottom: ["leftEdge", "bottomLeftEdge"], left: [""] },
-    rightEdge: { up: ["rightEdge", "topRightCorner"], right: [""], bottom: ["rightEdge", "bottomRightCorner"], left: ["leftEdge", "internal"] },
+    topLeftCorner: { up: [], right: ["topEdge", "topRightCorner"], bottom: ["leftEdge", "bottomLeftCorner"], left: [] },
+    topRighttCorner: { up: [], right: [], bottom: ["rightEdge", "bottomRightCorner"], left: ["topEdge", "topLeftCorner"] },
+    bottomRightCorner: { up: ["rightEdge", "topRightCorner"], right: [], bottom: [], left: ["bottomEdge", "bottomLeftCorner"] },
+    bottomLeftCorner: { up: ["leftEdge", "topLeftCorner"], right: ["bottomEdge", "bottomRightCorner"], bottom: [], left: [] },
+    topEdge: { up: [], right: ["topEdge", "topRightCorner"], bottom: ["bottomEdge", "internal"], left: ["topLeftCorner", "topEdge"] },
+    bottomEdge: { up: ["internal", "topEdge"], right: ["bottomRightCorner", "bottomEdge"], bottom: [], left: ["bottomEdge", "bottomLeftCorner"] },
+    leftEdge: { up: ["leftEdge", "topLeftCorner"], right: ["rightEdge", "internal"], bottom: ["leftEdge", "bottomLeftCorner"], left: [] },
+    rightEdge: { up: ["rightEdge", "topRightCorner"], right: [], bottom: ["rightEdge", "bottomRightCorner"], left: ["leftEdge", "internal"] },
     internal: { up: ["topEdge", "internal"], right: ["rightEdge", "internal"], bottom: ["bottomEdge", "internal"], left: ["leftEdge", "internal"] },
-    nothing: { up: [], right: [], bottom: [], left: [] }
 }
 
 export { rules, allTiles }
-export type { Sides, Tile, Rules }
+export type { Side, Tile, Rules }
