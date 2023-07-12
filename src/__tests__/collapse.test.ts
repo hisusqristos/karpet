bb3a6fd1fdaa8ad0e6bb8b1ee1483325a0f7c490
hisusqristos <varyadaser@gmail.com>
2023-07-12 12:19:04 +0400

    :axe: ij to xy

--- src/__tests__/collapse.test.ts
+++ src/__tests__/collapse.test.ts
@@ -1,43 +1,43 @@
 import { collapse } from "../helpers/collapse";
 
-describe.skip("collapse at given coordinates, with given value", () => {
+describe("collapse at given coordinates, with given value", () => {
   test("collapse at 0, 0", () => {
     const grid = [
       ["", "", ""],
       ["", "", ""],
     ];
-    const collapsedZeroZero = collapse("asd")(0, 0)(grid);
+    const collapsed = collapse("asd")(0, 0)(grid);
     const result = [
       ["asd", "", ""],
       ["", "", ""],
     ];
 
-    expect(result).toEqual(collapsedZeroZero);
+    expect(result).toEqual(collapsed);
   });
   test("collapse at 1, 2", () => {
     const grid = [
       ["", "", ""],
       ["", "", ""],
     ];
-    const collapsedZeroZero = collapse("asd")(1, 2)(grid);
+    const collapsed = collapse("asd")(1, 2)(grid);
     const result = [
       ["", "", ""],
       ["", "", "asd"],
     ];
 
-    expect(result).toEqual(collapsedZeroZero);
+    expect(result).toEqual(collapsed);
   });
   test("should not do anything if collapsed at non existing index 2, 2", () => {
     const grid = [
       ["", "", ""],
       ["", "", ""],
     ];
-    const collapsedZeroZero = collapse("asd")(2, 2)(grid);
+    const collapsed = collapse("asd")(2, 2)(grid);
     const result = [
       ["", "", ""],
       ["", "", ""],
     ];
 
-    expect(result).toEqual(collapsedZeroZero);
+    expect(result).toEqual(collapsed);
   });
 });
--- src/__tests__/investigate.test.ts
+++ src/__tests__/investigate.test.ts
@@ -5,7 +5,7 @@ import { rules, Side, Rules, Tile } from "../RULES";
 import { findKey } from "../helpers/findKey";
 
 describe.skip("investigate surrounding cells and return an array of possible tiles for the current cell", () => {
-  test("if betwen LEFT-EDGE and RIGHT-EDGE then INTERNAL", () => {
+  test.skip("if betwen LEFT-EDGE and RIGHT-EDGE then INTERNAL", () => {
     const blankGrid = gridInit(3, 1);
     const grid = pipe(
       blankGrid,
@@ -16,18 +16,42 @@ describe.skip("investigate surrounding cells and return an array of possible til
     const expectedResule = ["internal"];
     expect(investigate(rules)(1, 0)(grid)).toEqual(expectedResule);
   });
-  test("if above BOTTOM-EDGE then INTERNAL, TOP_EDGE", () => {
+  test.skip("if above BOTTOM-EDGE then INTERNAL, TOP_EDGE", () => {
     const blankGrid = gridInit(1, 2);
     const grid = pipe(blankGrid, collapse("bottomEdge")(0, 1));
 
     const expectedResule = ["internal", "topEdge"];
     expect(investigate(rules)(0, 0)(grid)).toEqual(expectedResule);
   });
+  test.only("find middle cell possibilities", () => {
+    const blankGrid = gridInit(3, 3);
+    const grid: Tile[][] = pipe(
+      blankGrid,
+      collapse("topLeftCorner")(0, 0),
+      collapse("topEdge")(1, 0),
+      collapse("topRightCorner")(2, 0),
+      collapse("leftEdge")(0, 1),
+      collapse("leftEdge")(1, 1),
+      collapse("rightEdge")(2, 1),
+      collapse("bottomLeftCorner")(0, 2),
+      collapse("bottomEdge")(1, 2),
+      collapse("bottomRightCorner")(2, 2)
+    );
+
+    // const grid: Tile[][] = [
+    //   ["topLeftCorner", "topEdge", "topRightCorner"],
+    //   ["leftEdge", "rightEdge", "rightEdge"],
+    //   ["bottomLeftCorner", "bottomEdge", "bottomRightCorner"],
+    // ];
+
+    expect(investigate(rules)(1, 1)(grid)).toEqual(["internal"]);
+  });
 });
 
 const investigate = (rules: Rules) => {
   return (y: number, x: number) => {
     return (grid: Tile[][]): Tile[] => {
+      console.log(grid);
       const surroundingTiles: Record<Side, Tile> = {
         bottom: grid[y - 1][x],
         left: grid[y][x + 1],
