cbe44bb705d22fec6c2d1c568b59a09a9dbcc781
hisusqristos <varyadaser@gmail.com>
2024-11-29 16:35:49 +0400

    :gear: hello baby elm ><

---
+++ src/Main.elm
@@ -0,0 +1,6 @@
+module Main exposing (..)
+
+import Html exposing (text, Html)
+
+main : Html msg
+main = text "բացառությամբ որմնախորշերի, ամեն ինչ հարթ էր, քանի որ ներսից տունն ամբողջությամբ ծածկված էր գորգերով կամ այլ նախշազարդ գործվածքներով"
