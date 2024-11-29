e3e4cf5d465c074dfef7fb733d50a41179558594
hisusqristos <varyadaser@gmail.com>
2024-11-29 14:14:49 +0400

    :fire: burngit add .git add .git add .git add .!

--- src/helpers/gridInit.ts
+++
@@ -1,7 +0,0 @@
-const gridInit = (width: number, height: number): string[][] => {
-    let row: string[] = new Array(width).fill("")
-    const grid = new Array(height).fill(row);
-    return grid;
-};
-
-export { gridInit }
--- src/helpers/pipe.ts
+++
@@ -1,4 +0,0 @@
-const pipe = (value: any, ...fns: Function[]) =>
-    fns.reduce((acc, fn) => fn(acc), value);
-
-export { pipe }
